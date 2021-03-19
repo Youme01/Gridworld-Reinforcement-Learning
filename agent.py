e7b7490cd28f4c2c98854bcf294ac31f14e1f3cb
Youme01 <51056070+Youme01@users.noreply.github.com>
2021-03-20 00:55:31 +0600

    init commit

---
+++ agent.py
@@ -0,0 +1,114 @@
+#IMPORTS
+import torch
+import random
+import numpy as np
+from collections import deque
+from game import GameAI
+from model import Linear_QNet, QTrainer
+from GraphPlot import plot
+
+#VARIABLES
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+LR = 0.001
+num_episode = 50000
+SHOW_EVERY = 200
+final_move = [0,0,0,0]
+
+class Agent:
+
+    def __init__(self):
+        self.n_games = 0
+        self.epsilon = 0 # randomness
+        self.gamma = 0.9 # discount rate
+        self.memory = deque(maxlen=MAX_MEMORY) # popleft()
+        self.model = Linear_QNet(4, 256, 4)
+        self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
+
+    #TO DO
+    def get_state(self, game):
+        drone = game.drone
+        state = [
+            drone.x,drone.y,
+            game.man.y < game.drone.y,  # man up
+            game.man.y > game.drone.y # man down
+        ]
+        return np.array(state, dtype=int)
+    
+    # Random Moves: tradeoff exploration / exploitation
+    def get_action(self, state):
+        self.epsilon = 80 - self.n_games
+
+        if random.randint(0, 200) < self.epsilon:
+            move = random.randint(0, 3)
+            final_move[move] = 1
+        else:
+            state0 = torch.tensor(state, dtype=torch.float)
+            prediction = self.model(state0)
+            move = torch.argmax(prediction).item()
+            final_move[move] = 1
+
+        return final_move
+       
+    #Storing Memory
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done)) # popleft if MAX_MEMORY is reached
+
+    #TO DO
+    def train_long_memory(self):
+
+        if len(self.memory) > BATCH_SIZE:
+            mini_sample = random.sample(self.memory, BATCH_SIZE) # list of tuples
+        else:
+            mini_sample = self.memory
+
+        states, actions, rewards, next_states, dones = zip(*mini_sample)
+        self.trainer.train_step(states, actions, rewards, next_states, dones)
+
+    # Updating Q Values
+    def train_short_memory(self, state, action, reward, next_state, done):
+        self.trainer.train_step(state, action, reward, next_state, done)
+
+
+# MAIN FUNCTION (RunfromHere)
+def train():
+
+    plot_reward = []
+    plot_mean_reward = []
+    total_reward = 0
+
+    agent = Agent()
+    game = GameAI()
+
+    for episode in range(num_episode):
+
+        state_old = agent.get_state(game) # get old state
+
+        final_move = agent.get_action(state_old) # get move
+
+        reward, done, score = game.play_step(final_move) # perform move and get new state
+
+        state_new = agent.get_state(game)
+
+        # train short memory
+        agent.train_short_memory(state_old, final_move, reward, state_new, done)
+
+        # remember
+        agent.remember(state_old, final_move, reward, state_new, done)
+
+         # train long memory, plot result
+        if done:
+            game.reset()
+            agent.n_games += 1
+            agent.train_long_memory()
+            
+        print('Episode:', episode,'Reward', reward,'Game', agent.n_games)
+
+    plot_reward.append(reward)
+    total_reward += reward
+    mean_score = total_reward / agent.n_games
+    plot_mean_reward.append(mean_score)
+    plot(plot_reward, plot_mean_reward)         
+          
+if __name__ == '__main__':
+    train()
---
+++ game.py
@@ -0,0 +1,128 @@
+#IMPORTS
+import pygame
+import random
+from enum import Enum
+from collections import namedtuple
+import numpy as np
+
+#INITIALIZATION
+pygame.init()
+font = pygame.font.Font('arial.ttf', 25)
+Point = namedtuple('Point', 'x, y')
+
+# COLOR CODES
+WHITE = (255, 255, 255)
+RED = (200,0,0)
+BLUE1 = (0, 0, 255)
+BLUE2 = (0, 100, 255)
+BLACK = (0,0,0)
+
+#VARIABLES
+BLOCK_SIZE = 20
+SPEED = 20
+WIDTH = 200 
+HEIGHT = 200
+
+class GameAI:
+
+    def __init__(self, w=WIDTH, h=HEIGHT):
+
+        self.w = w
+        self.h = h
+
+        self.display = pygame.display.set_mode((self.w, self.h)) # init display
+        pygame.display.set_caption('SAR')
+
+        self.clock = pygame.time.Clock()
+        self.reset()
+
+    # Reset World
+    def reset(self):
+        self.drone = Point(self.w/2, self.h/2)
+        self.score = 0
+        self.man = None
+        self._place_man()
+        self.frame_iteration = 0
+
+    # Man Position
+    def _place_man(self):
+        x =self.w // 2
+        y = self.h //2
+        self.man = Point(x, y)
+    
+    # Updating reward after every step
+    def play_step(self, action):
+        self.frame_iteration += 1
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        
+        # 2. move drone
+        self._move(action) 
+
+        # 3. check if game over
+        game_over = False
+
+        # 4. reward for each step
+        self.reward, game_over, self.score = self.get_reward(game_over)   
+       
+        # 5. update ui and clock
+        self._update_ui()
+
+        self.clock.tick(SPEED)
+
+        return self.reward, game_over, self.score
+
+    # Hits boundary
+    def is_collision(self, pt=None):
+        if pt is None:
+            pt = self.drone
+        if pt.x > self.w - BLOCK_SIZE or pt.x < 0 or pt.y > self.h - BLOCK_SIZE or pt.y < 0:
+            return True
+        return False
+
+    #TO DO
+    def get_reward(self,game_over):
+
+        if self.is_collision() or self.frame_iteration > 1000: # colision & loop 
+            game_over = True
+            self.reward = -500
+            return self.reward, game_over, self.score
+
+        elif self.drone == self.man: # place new man or just move
+            self.score += 1
+            self.reward = 1000
+            self._place_man() 
+
+        else:
+            self.reward = 0
+
+        return self.reward, game_over, self.score
+
+    #Update Display
+    def _update_ui(self):
+        self.display.fill(BLACK)
+
+        pygame.draw.rect(self.display, RED, pygame.Rect(self.man.x, self.man.y, BLOCK_SIZE, BLOCK_SIZE))
+        pygame.draw.rect(self.display, BLUE1, pygame.Rect(self.drone.x, self.drone.y, BLOCK_SIZE, BLOCK_SIZE))
+
+        pygame.display.flip()
+
+    # Actions: based on direction updte the drone
+    def _move(self, action):
+
+        x = self.drone.x
+        y = self.drone.y
+
+        if np.array_equal(action, [1, 0, 0,0]):
+            x += BLOCK_SIZE  # right
+        elif np.array_equal(action, [0, 1, 0,0]):
+            x -= BLOCK_SIZE # left
+        elif np.array_equal(action, [0, 0, 1,0]): # [0, 0, 1]
+            y += BLOCK_SIZE # down
+        else:
+            y -= BLOCK_SIZE # up
+   
+        self.drone = Point(x, y)
